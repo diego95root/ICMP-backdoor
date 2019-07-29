04ac73dd31e764bd0789db6bb1942d908eb48234
diego95root <minidiego@ono.com>
2019-07-29 15:53:23 +0200

    Added argparse to server and writing to file option

--- receiver.py
+++ receiver.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-import sys, base64
+import sys, base64, argparse
 
 def raw(p):
     return p[ICMP][Raw].load[16:32]
@@ -7,12 +7,19 @@ def raw(p):
 def isICMP(p):
     return p.haslayer(ICMP) and p[ICMP].type==8
 
+def write(data, filename):
+    f = open(filename, "w")
+    f.write(data)
+    f.close()
+
 def serverLastBytes(interface):
 
     try:
         config.conf.iface = interface
     except:
         pass
+
+    received = []
 
     for x in range(4):
 
@@ -28,9 +35,13 @@ def serverLastBytes(interface):
             buf = [buf[i] for i in range(len(buf)) if i % 2 == 0]
 
         buf = ''.join(buf)
+        dec = base64.b64decode(buf[:buf.find('\n')])
 
         print "[*] Buf length   : {}".format(len(buf.encode("hex")))
-        print "[*] Received data: \"{}\"".format(base64.b64decode(buf[:buf.find('\n')]))
+        print "[*] Received data: \"{}\"".format(dec)
+        received.append(dec)
+
+    return "\n".join(received) + "\n"
 
 def serverTimeBased(interface):
 
@@ -38,6 +49,8 @@ def serverTimeBased(interface):
         config.conf.iface = interface
     except:
         pass
+
+    received = []
 
     for x in range(4):
 
@@ -57,10 +70,29 @@ def serverTimeBased(interface):
             msg += (diff- (1 * (diff % 2 != 0)))/2 * "0" +  ("1" * (diff % 2 != 0))
 
         data = hex(int(msg, 2))[2:].replace("L", "").decode("hex")
+        received.append(data)
+
         print "[*] Received data: \"{}\"".format(data)
+
+    return "\n".join(received) + "\n"
 
 if __name__ == "__main__":
 
-    interface = "lo"
+    parser = argparse.ArgumentParser(description='Client that sends data disguised in ICMP packets. Keep in mind settings need to be the same for the server')
+    parser.add_argument('-i', '--interface', type=str, help='interface to listen on')
+    parser.add_argument('-o', '--out', type=str, help='write received data on a file')
+    parser.add_argument('-m', '--mode', type=int, default=1, help='the mode of exfiltration: 1 is lousy (inside packets), 2 time-based')
+    args = parser.parse_args()
 
-    serverTimeBased(interface)
+    # make interface necessary
+    print "[*] Started listener on interface: {}".format(args.interface)
+    print "[*] Listening mode: {}".format(args.mode)
+
+    if args.mode == 1:
+        out = serverLastBytes(args.interface)
+    elif args.mode == 2:
+        out = serverTimeBased(args.interface)
+
+    if args.out:
+        write(out, args.out)
+        print "[*] Exfiltrated data saved to: {}".format(args.out)
