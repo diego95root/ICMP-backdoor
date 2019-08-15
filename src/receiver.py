3331837d1fa25e984030207f5c91a8f567d05145
diego95root <minidiego@ono.com>
2019-08-15 17:19:58 +0200

    Restructured code inside receiver, added communication functions

--- src/receiver.py
+++ src/receiver.py
@@ -12,59 +12,76 @@ def write(data, filename):
     f.write(data)
     f.close()
 
-def serverLastBytes(interface):
+def timeBasedCommunication(interface):
+
+    s = sniff(iface=interface, lfilter = lambda packet : isICMP(packet),
+              stop_filter = lambda packet : isICMP(packet) and '\n' in raw(packet))
+
+    # if interface is loopback for testing purposes,
+    # each packet is duplicated so we need to remove it
+
+    if interface == "lo":
+        s = [s[i] for i in range(len(s)) if i % 2 == 0]
+
+    msg = "1"
+
+    for n in range(1, len(s)):
+        diff = int((s[n].time - s[n-1].time) * 10)
+        msg += (diff- (1 * (diff % 2 != 0)))/2 * "0" +  ("1" * (diff % 2 != 0))
+
+    data = hex(int(msg, 2))[2:].replace("L", "").decode("hex")
+
+    return data
+
+def lastBytesCommunication(interface):
+    s = sniff(iface=interface, lfilter = lambda packet : isICMP(packet),
+              stop_filter = lambda packet : isICMP(packet) and '\n' in raw(packet))
+
+    buf = [raw(i) for i in s]
+
+    # if interface is loopback for testing purposes,
+    # each packet is duplicated so we need to remove it
+
+    if interface == "lo":
+        buf = [buf[i] for i in range(len(buf)) if i % 2 == 0]
+
+    buf = ''.join(buf)
+    dec = base64.b64decode(buf[:buf.find('\n')])
+
+    return dec
+
+def server(interface, mode):
 
     received = []
 
     for x in range(4):
 
-        s = sniff(iface=interface, lfilter = lambda packet : isICMP(packet),
-                  stop_filter = lambda packet : isICMP(packet) and '\n' in raw(packet))
-
-        buf = [raw(i) for i in s]
-
-        # if interface is loopback for testing purposes,
-        # each packet is duplicated so we need to remove it
-
-        if interface == "lo":
-            buf = [buf[i] for i in range(len(buf)) if i % 2 == 0]
-
-        buf = ''.join(buf)
-        dec = base64.b64decode(buf[:buf.find('\n')])
-
-        print "[*] Buf length   : {}".format(len(buf.encode("hex")))
-        print "[*] Received data: \"{}\"".format(dec)
-        received.append(dec)
-
-    return "\n".join(received) + "\n"
-
-def serverTimeBased(interface):
-
-    received = []
-
-    for x in range(4):
-
-        s = sniff(iface=interface, lfilter = lambda packet : isICMP(packet),
-                  stop_filter = lambda packet : isICMP(packet) and '\n' in raw(packet))
-
-        # if interface is loopback for testing purposes,
-        # each packet is duplicated so we need to remove it
-
-        if interface == "lo":
-            s = [s[i] for i in range(len(s)) if i % 2 == 0]
-
-        msg = "1"
-
-        for n in range(1, len(s)):
-            diff = int((s[n].time - s[n-1].time) * 10)
-            msg += (diff- (1 * (diff % 2 != 0)))/2 * "0" +  ("1" * (diff % 2 != 0))
-
-        data = hex(int(msg, 2))[2:].replace("L", "").decode("hex")
-        received.append(data)
+        if mode == 1:
+            data = lastBytesCommunication(interface)
+        elif mode == 2:
+            data = timeBasedCommunication(interface)
 
         print "[*] Received data: \"{}\"".format(data)
+        received.append(data)
 
     return "\n".join(received) + "\n"
+
+
+def pwnShell():
+
+    #function will be called on each script
+    """
+
+    client connects to server
+
+    server sends command as client, client replies sending back data
+
+    # both need to send and receive (communication both ways)
+
+    """
+
+    pass
+
 
 if __name__ == "__main__":
 
@@ -80,11 +97,8 @@ if __name__ == "__main__":
     print "[*] Started listener on interface: {}".format(args.interface)
     print "[*] Listening mode: {}".format(args.mode)
 
-    if args.mode == 1:
-        out = serverLastBytes(args.interface)
-    elif args.mode == 2:
-        out = serverTimeBased(args.interface)
-
+    out = server(args.interface, args.mode)
+    
     if args.out:
         write(out, args.out)
         print "[*] Exfiltrated data saved to: {}".format(args.out)
